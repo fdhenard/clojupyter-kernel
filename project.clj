3d52e3457f624fa2d0659838f58cff1100971ef6
Frank Henard <fdhenard@gmail.com>
2021-05-11 11:09:22 -0500

    cleanup

--- project.clj
+++ project.clj
@@ -4,8 +4,5 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [clojupyter "0.3.2"]
-                 #_[seancorfield/depstar ""]]
-  :repl-options {#_#_:init-ns clojupyter-kernel.core
-                 :init-ns user}
-  )
+                 [clojupyter "0.3.2"]]
+  :repl-options {:init-ns user})
